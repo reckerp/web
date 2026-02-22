bf8e9ae8c9f49b189b34dc3d238bb33241ab1819
reckerp <git@recker.io>
2026-02-22 12:30:11 +0100

    feat: implement landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://recker.io',
+  integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare(),
+});
